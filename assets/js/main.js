46bad8ba9b7ae0d70515ec167e5ceed5cefcd083
thomas <thomaalanstevens@gmail.com>
2022-07-02 12:32:03 -0700

    Added botw app gif, additional text, and fixed NASA Photo Browser text overflow.

--- assets/js/main.js
+++ assets/js/main.js
@@ -394,3 +394,189 @@
 					});
 
 })(jQuery);
+
+// NASA Photo Browser Code
+
+document.querySelector('.submit').addEventListener('click', getPhotoObj)
+
+async function getPhotoObj(){
+    date = document.querySelector('.date').value
+    try {
+        const resp = await fetch(`https://api.nasa.gov/planetary/apod?api_key=VxIoUMlLc3MRr7kYX3WaUvlBoPvIVso8APEeBKne&date=${date}`)
+        const data = await resp.json()  
+		if(data['code'] == 400){
+			document.querySelector('.nasaPhoto').src = '/images/NASA.png'
+			document.querySelector('.nasaInfo').innerText = `Error: ${data['msg']}`
+		}
+		else{
+			document.querySelector('.nasaPhoto').src = data['hdurl']
+			document.querySelector('.nasaInfo').innerText = data['explanation']
+		}
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+document.querySelector('.nasaInfoButton').addEventListener('click', showHideOverflow)
+
+function showHideOverflow(){
+	document.querySelector('.nasaInfo').classList.toggle('hideOverflow')
+}
+
+
+// Demacia vs. Noxus 
+
+//Reset board
+let resetButton = document.querySelector('.reset').addEventListener('click', refresh)
+let nextButton1 = document.querySelector('.nextBattleV').addEventListener('click', refresh)
+let nextButton2 = document.querySelector('.nextBattleD').addEventListener('click', refresh)
+let nextButton3 = document.querySelector('.nextBattleT').addEventListener('click', refresh)
+let turn = 0
+
+function refresh(){
+	document.querySelector('.demaciaVsNoxusTurn').innerText = `Choose a side!`
+	document.querySelector('.chooseSide').style = 'display: flex;'	
+	document.querySelector('.board').style = 'display: none;'
+	document.querySelector('.victory').style = 'display: none;'
+	document.querySelector('.defeat').style = 'display: none;'
+	document.querySelector('.tie').style = 'display: none;'
+	document.querySelectorAll('.dvnSpacing').forEach(div => div.innerHTML = '')
+	turn = 0
+}
+
+
+//Pick a side
+let noxusButton = document.querySelector('#NOXUS').addEventListener('click', pickSide)
+let demaciaButton = document.querySelector('#DEMACIA').addEventListener('click', pickSide)
+let side = ''
+
+function pickSide(){
+	side = this.innerText
+	document.querySelector('.demaciaVsNoxusTurn').innerText = `${side}'s CHANCE TO ATTACK`
+	document.querySelector('.chooseSide').style = 'display: none;'
+	document.querySelector('.board').style = 'display: flex;'
+	side == 'DEMACIA' ? turn = 0 : turn = 1
+}
+
+
+
+//Play game
+let placeTokenButtons = document.querySelectorAll('.dvnSpacing')
+placeTokenButtons.forEach(button => button.addEventListener('click', placeToken))
+
+function placeToken(){
+	let demaciaToken = document.createElement('img')
+	demaciaToken.src = '/images/godKingGaren.png'
+	demaciaToken.classList.add('image', 'fit')
+	demaciaToken.style = 'object-fit: cover; margin: 0;'
+
+	let noxusToken = document.createElement('img')
+	noxusToken.src = '/images/godKingDarius.jpg'
+	noxusToken.classList.add('image', 'fit')
+	noxusToken.style = 'object-fit: cover; margin: 0;'
+
+	if(this.hasChildNodes() == false && turn % 2 == 0){
+		this.append(demaciaToken)
+		turn ++
+		document.querySelector('.demaciaVsNoxusTurn').innerText = `Noxus's CHANCE TO ATTACK`
+		this.setAttribute('data-attribute', 'DEMACIA')
+	}
+	else if(this.hasChildNodes() == false && turn % 2 == 1){
+		this.append(noxusToken)
+		turn++
+		document.querySelector('.demaciaVsNoxusTurn').innerText = `Demacia's CHANCE TO ATTACK`
+		this.setAttribute('data-attribute', 'NOXUS')
+	}
+
+	if(determineWinner(this) != null){
+		if(determineWinner(this) == side){
+			document.querySelector('.demaciaVsNoxusTurn').innerText = ``
+			document.querySelector('.chooseSide').style = 'display: none;'	
+			document.querySelector('.board').style = 'display: none;'
+			document.querySelector('.victory').style = 'display: block;'
+			document.querySelector('.defeat').style = 'display: none;'
+			document.querySelector('.tie').style = 'display: none;'
+			Array.from(document.querySelector('.board').childNodes).filter((elem, ind, arr) => ind % 2 == 1).slice(0,9).forEach(space => space.removeAttribute('data-attribute'))
+		}
+		else if(determineWinner(this) == 'There were no winners in this war...'){
+			document.querySelector('.demaciaVsNoxusTurn').innerText = ``
+			document.querySelector('.chooseSide').style = 'display: none;'	
+			document.querySelector('.board').style = 'display: none;'
+			document.querySelector('.victory').style = 'display: none;'
+			document.querySelector('.defeat').style = 'display: none;'
+			document.querySelector('.tie').style = 'display: block;'
+			Array.from(document.querySelector('.board').childNodes).filter((elem, ind, arr) => ind % 2 == 1).slice(0,9).forEach(space => space.removeAttribute('data-attribute'))
+		}
+		else{
+			document.querySelector('.demaciaVsNoxusTurn').innerText = ``
+			document.querySelector('.chooseSide').style = 'display: none;'	
+			document.querySelector('.board').style = 'display: none;'
+			document.querySelector('.victory').style = 'display: none;'
+			document.querySelector('.defeat').style = 'display: block;'
+			document.querySelector('.tie').style = 'display: none;'
+			Array.from(document.querySelector('.board').childNodes).filter((elem, ind, arr) => ind % 2 == 1).slice(0,9).forEach(space => space.removeAttribute('data-attribute'))
+		}
+
+		// if(determineWinner(this) == side){
+		// 	document.querySelector('.demaciaVsNoxusTurn').innerText = ``
+		// 	document.querySelector('.chooseSide').style = 'display: none;'	
+		// 	document.querySelector('.board').style = 'display: none;'
+		// 	document.querySelector('.victory').style = 'display: block;'
+		// 	document.querySelector('.defeat').style = 'display: none;'
+		// 	document.querySelector('.tie').style = 'display: none;'
+		// 	Array.from(document.querySelector('.board').childNodes).filter((elem, ind, arr) => ind % 2 == 1).slice(0,9).forEach(space => space.removeAttribute('data-attribute'))
+		// }
+		// else if(determineWinner(this) = 'There were no winners in this war...'){
+		// 	document.querySelector('.chooseSide').style = 'display: none;'	
+		// 	document.querySelector('.board').style = 'display: none;'
+		// 	document.querySelector('.victory').style = 'display: none;'
+		// 	document.querySelector('.defeat').style = 'display: none;'
+		// 	document.querySelector('.tie').style = 'display: block;'
+		// 	Array.from(document.querySelector('.board').childNodes).filter((elem, ind, arr) => ind % 2 == 1).slice(0,9).forEach(space => space.removeAttribute('data-attribute'))
+		// }
+		// else{
+		// 	document.querySelector('.demaciaVsNoxusTurn').innerText = ``
+		// 	document.querySelector('.chooseSide').style = 'display: none;'	
+		// 	document.querySelector('.board').style = 'display: none;'
+		// 	document.querySelector('.victory').style = 'display: none;'
+		// 	document.querySelector('.defeat').style = 'display: block;'
+		// 	document.querySelector('.tie').style = 'display: none;'
+		// 	Array.from(document.querySelector('.board').childNodes).filter((elem, ind, arr) => ind % 2 == 1).slice(0,9).forEach(space => space.removeAttribute('data-attribute'))
+		// }
+		
+	}
+
+}
+
+
+//determine if there was a winner.
+function determineWinner(){
+	let board = document.querySelector('.board').childNodes
+	board = Array.from(board).filter((elem, ind, arr) => ind % 2 ==	  1).slice(0,9)
+	let boardState = board.map(space => space.getAttribute('data-attribute'))
+
+	switch(true){
+		case boardState[0] == boardState[1] && boardState[1] == boardState[2] && boardState[1] != null:
+			return boardState[1]
+		case boardState[3] == boardState[4] && boardState[4] == boardState[5] && boardState[4] != null:
+			return boardState[4]
+		case boardState[6] == boardState[7] && boardState[7] == boardState[8] && boardState[7] != null:
+			return boardState[7]
+		case boardState[0] == boardState[3] && boardState[3] == boardState[6] && boardState[3] != null:
+			return boardState[3]
+		case boardState[1] == boardState[4] && boardState[4] == boardState[7] && boardState[4] != null:
+			return boardState[4]
+		case boardState[2] == boardState[5] && boardState[5] == boardState[8] && boardState[5] != null:
+			return boardState[5]
+		case boardState[0] == boardState[4] && boardState[4] == boardState[8] && boardState[4] != null:
+			return boardState[4]
+		case boardState[2] == boardState[4] && boardState[4] == boardState[6] && boardState[4] != null:
+			return boardState[4]
+		case boardState.includes(null):
+			break;
+		default:
+			return 'There were no winners in this war...'
+	}
+
+}
